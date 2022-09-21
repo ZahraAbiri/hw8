ea745cff736f810f525041c32e49c08e4d9ac5ba
ali <zahraabiri816@gmail.com>
2022-09-21 20:46:26 +0430

    write menue

--- Subway.py
+++ Subway.py
@@ -1,4 +1,6 @@
 import datetime
+
+
 class Card:
     def __init__(self, serial_number):
         self.serial_number = serial_number
@@ -36,8 +38,6 @@ class Time_credit_card(One_time_use_card):
         self.start_date = start_date
         self.end_date = None
 
-
-
     def get_start_date(self):
         return self.start_date
 
@@ -47,28 +47,74 @@ class Time_credit_card(One_time_use_card):
     def get_end_date(self):
         return self.end_date
 
-    def set_money(self, end_date):
+    def set_end_date(self, end_date):
         self.end_date = end_date
 
-def compareDate(start,end):
+
+def compareDate(start, end):
     first_date = datetime.date(int(start[:4]), int(start[5:7]), int(start[8:10]))
     second_date = datetime.date(end.year, end.month, end.day)
     print(first_date)
     print(second_date)
     result = first_date > second_date
     return result
-def check_date_card():
-    previous=datetime.datetime.today()-datetime.timedelta(days=1)
-    print(previous)
 
-# one_time = One_time_use_card(2500, 140111)
-# credit = Credit_card(14000, 140112)
-# check_date_card()
-# timedelta = datetime.timedelta(days=1)
-# today = datetime.datetime.today()
+
+def check_date_card():
+    previous = datetime.datetime.today() - datetime.timedelta(days=1)
+    return previous
+
+
+one_time = One_time_use_card(2500, 140111)
+credit = Credit_card(14000, 140112)
+time_credit = Time_credit_card(12000, 140113, datetime.date)
+time_credit.set_end_date(check_date_card())
 date = compareDate(str(datetime.datetime.today()), datetime.datetime.today() - datetime.timedelta(days=1))
 
 print(date)
-# start="2022-09-20 18:52:02.655069"
-# date = datetime.date(int(start[:4]), int(start[5:7]), int(start[8:10]))
-# print(date,'----')
+list_One_time=[]
+list_credit=[]
+list_time_credit=[]
+list_One_time.append(one_time)
+list_credit.append(credit)
+list_time_credit.append(time_credit)
+money_for_one_trip=2500
+menu_description = """
+
+1. use card
+2. increase money
+3. exit
+"""
+print(menu_description)
+option = int(input('Choose one option'))
+while option != 3:
+        if option == 1:
+
+            serial = int(input('enter serial number'))
+            for i in list_One_time:
+                if i.serial_number == serial and i.get_money() > 0:
+                    i.set_money(0)
+                else:
+                    print('this card used before')
+            for i in list_time_credit:
+                if i.serial_number == serial:
+                    if compareDate(str(datetime.datetime.today()), datetime.datetime.today() - datetime.timedelta(days=1)):
+                        print('card time expired')
+                    else:
+                        i.set_money(i.get_money() - money_for_one_trip)
+            for i in list_credit:
+                if i.serial_number == serial:
+                    i.set_money(i.money - money_for_one_trip)
+
+        elif option == 2:
+                serial = int(input('enter serial number'))
+                for i in list_credit:
+                    if i.serial_number == serial:
+                        i.set_money(i.money + (money_for_one_trip*10))
+                        print("your credit is "+i.get_money())
+
+        else:
+            print(f"Invalid option-Choose a number between 1 to 3")
+            # continue
+        option = int(input(f"Choose an option from list by entering number 1 to 3\n"
+                           f"{menu_description}"))
