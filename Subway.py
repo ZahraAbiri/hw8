eed6c6be6a6a59ab778966155d2062b80d9a8a88
ali <zahraabiri816@gmail.com>
2022-09-21 18:25:34 +0430

    create class card and feature serial number

--- Subway.py
+++ Subway.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+class Card:
+    def __init__(self, serial_number):
+        self.serial_number = serial_number
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def get_serial_number(self):
+        return self.serial_number
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def set_serial_number(self, serial_no):
+        self.serial_number = serial_no
